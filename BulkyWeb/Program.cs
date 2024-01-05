cf839a6211d84636d66183617b96ab0703499212
zaid3251 <azaid3251@gmail.com>
2024-01-05 20:59:15 +0300

    Add Connection String

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,6 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
+using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
